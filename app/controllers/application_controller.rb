88605f9447f8be7e98e90fa461fb682c13e5f60a
Elias Ventura <eliasventura23@gmail.com>
2019-03-11 13:59:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,12 +20,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    user = User.find(session[:user_id])
-    if user
-      erb :account
-    end
-    erb :error
-  end
+     @current_user = User.find_by_id(session[:user_id])
+     if @current_user
+       erb :account
+     else
+       erb :error
+     end
+   end
 
   get '/logout' do
     session.clear

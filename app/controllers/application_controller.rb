c13ca03ac5c8ad4458481906ff89fba5b2f58669
Elias Ventura <eliasventura23@gmail.com>
2019-03-11 14:11:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if Helpers.is_logged_in?(session)
+    if User.find_by_id(session[:user_id])
       erb :account
     else
     erb :error

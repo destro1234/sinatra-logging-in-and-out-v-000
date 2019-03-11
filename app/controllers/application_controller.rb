15d88ea51e03c936a177e33e149f18a00306f4c6
Elias Ventura <eliasventura23@gmail.com>
2019-03-11 14:28:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,9 +20,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    user = User.find_by_id(session[:user_id])
-
-    if user!= nil
+    if User.find_by_id(session[:user_id])
       erb :account
     else
     erb :error
